81d91b111ea45393a9262aa725ef1eecbb7549e0
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2014-01-26 15:03:55 -0300

    docstrings, first version of scrape-categories, save-categories

--- src/lazada/core.clj
+++ src/lazada/core.clj
@@ -16,42 +16,59 @@
 (def SUBCATEGORY_CLASS "fct-category")
 (def PRODUCT_CLASS "unit")
 
-(defn tree-for [url]
-  (-> (client/get url) :body parse as-hickory))
+(defn tree-for
+  "Retrieves the content of a given URL and parses them in a hickory-based tree"
+  [url]
+  (do
+    (println (str "GET " url))
+    (-> (client/get url) :body parse as-hickory)))
 
 (def homepage-tree (tree-for HOMEPAGE_URL))
 
-(defn full-url [uri]
-  (str HOMEPAGE_URL uri))
+(defn full-url
+  "Prepends the server homepage URL to a given URI if needed"
+  [uri]
+  (if (.startsWith uri "http://")
+    uri
+    (str HOMEPAGE_URL uri)))
 
 (def categories
-  (let [tree (tree-for HOMEPAGE_URL)
+  "Vector with the base categories of www.lazada.com.ph"
+  (let [tree homepage-tree
         category-nodes (s/select (s/class CATEGORY_CLASS) tree)]
     (vec
-    (map
-     #(hash-map :href (-> % :attrs :href)
-                :name (string/trim (-> % :content second :content first)))
-     category-nodes))))
+     (map
+      #(hash-map :url (-> % :attrs :href full-url)
+                 :name (string/trim (-> % :content second :content first)))
+      category-nodes))))
 
-(defn subcategories [tree]
+(defn subcategories
+  "Returns a vector of maps for each sub-category on the current category"
+  [tree]
   (let [links (s/select (s/child (s/class "selected") (s/tag :ul) (s/tag :li) (s/tag :a)) tree)]
     (vec
      (map
-      #(hash-map :url (-> % :attrs :href)
+      #(hash-map :url (-> % :attrs :href full-url)
                  :name (-> % :content first :content first))
       links))))
 
-(defn product-price* [node]
+(defn product-price*
+  "Returns the price of a given product in its original format"
+  [node]
   (let [deep (-> node :content (nth 3) :content butlast last :content)]
     (-> deep butlast last :content first string/trim)))
 
-(defn product-price [node]
+(defn product-price
+  "Returns a the price of a product as a Number"
+  [node]
   (let [formatted-price (product-price* node)
         formatted-amount ((string/split formatted-price #" ") 1)
         decimal-format (java.text.NumberFormat/getInstance java.util.Locale/UK)]
         (.parse decimal-format formatted-amount)))
 
-(defn top5-products [cat-tree]
+(defn top5-products
+  "Returns a vector of maps describing up to 5 products in a category"
+  [cat-tree]
   (let [product-nodes (-> (s/select (s/child (s/class "unit")) cat-tree))]
     (vec
      (map
@@ -61,12 +78,30 @@
           :sku   (-> e :content (nth 3) :content second :content second :attrs :data-sku-simple)
           :url   (-> e :attrs :href full-url)
           :price (product-price e)
-          :fmt-price (product-price* e)
+          ;; :fmt-price (product-price* e)
           :name  (-> e :attrs :title)))
       (take 5 product-nodes)))))
 
 (defn first-product [cat-tree]
   (first (-> (s/select (s/child (s/class "unit")) cat-tree))))
+
+(defn scrape-categories
+  "Given a map with :name and :url keys, finds subcategories and walks them down recursively, selecting the top-5 products in the way"
+  [m]
+  (let [cat-tree (-> m :url full-url tree-for)
+        products (top5-products cat-tree)
+        subcats (subcategories cat-tree)
+        children (vec (map scrape-categories subcats))]
+    (if (empty? children)
+      {:name (:name m) :url (:url m) :products products}
+      {:name (:name m) :url (:url m) :products products :children children})))
+
+(defn save-categories!
+  "Serializes a map a JSON string and saves it as the file resources/public/js/categories.json"
+  [m]
+  (let [output (json/write-str m)]
+    (spit "resources/public/js/categories.json" output)))
+
 
 ;; (def laptop-subtree
 ;;   (-> (s/select (s/class "fct-category") laptop-tree)
@@ -81,10 +116,12 @@
 (defn -main [& args]
   (if (empty? args)
     (do
-      (println "Usage: java -jar lazada-scaper.jar <COMMAND>\n")
+      (println "Usage: lein run <COMMAND>\n")
       (println "Commands:")
-      (println "scrape - Scrapes content of lazada.com.ph, generates resources/js/categories.json")
-      (println "server - Runs jetty locally and launches a browser to visualize categories"))))
+      (println "scrape  Scrapes content of www.lazada.com.ph, generates resources/js/categories.json")
+      (println "server  Runs Jetty locally and launches a browser to visualize categories")
+      (println))
+    (println (str "args: " (apply str args)))))
 
 ;; (defn -main [& args]
 ;;   (println "loading...")
